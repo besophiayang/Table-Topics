075b7397fd35069138ad37beeed51602a7c9f219
Tiffany Yang <betiffanyyang@gmail.com>
2023-03-04 15:48:48 -0800

    Initial Commit

---
+++ WiCHacksApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WiCHacksApp
+//
+//  Created by Sophia Yang on 2023-03-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
